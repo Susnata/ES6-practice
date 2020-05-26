8b490ec2f3f42c048ba32fd669716c32498ed3a3
Susnata Dube <susnata03@gmail.com>
2020-05-26 12:15:56 +0530

    Complete ES6 L1 ch1

--- script1.js
+++
@@ -1,4 +0,0 @@
-const d = new Date();
-const heading = document.createElement('h1');
-heading.innerHTML = "Today's date is " + d.toLocaleDateString();
-document.body.appendChild(heading);
---
+++ scripts/script1.js
@@ -0,0 +1,4 @@
+const d = new Date();
+// const heading = document.createElement('h1');
+document.body.innerHTML = "<h1>Today's date is " + d.toLocaleDateString() + "</h1>";
+// document.body.appendChild(heading);
